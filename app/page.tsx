d5f9080840bb30039599cff47d53dd99cd08ff3d
Techie <118513185+techieontour@users.noreply.github.com>
2023-09-28 11:01:57 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <button className="btn btn-primary btn-outline ">Add to Cart</button>
+    </main>
+  );
+}
